b4b2d1d95e196795a2f6c5ca69e696bdb2624120
SeonJeong <sjkim5621@naver.com>
2024-07-10 13:49:41 +0900

    [2] feat : splash 및 intro 세팅

---
+++ app/src/main/java/com/example/koview/presentation/ui/intro/login/LoginFragment.kt
@@ -0,0 +1,8 @@
+package com.example.koview.presentation.ui.intro.login
+
+import com.example.koview.R
+import com.example.koview.databinding.FragmentLoginBinding
+import com.example.koview.presentation.base.BaseFragment
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+}
---
+++ app/src/main/java/com/example/koview/presentation/ui/intro/signup/complete/SignUpCompleteFragment.kt
@@ -0,0 +1,10 @@
+package com.example.koview.presentation.ui.intro.signup.complete
+
+import com.example.koview.R
+import com.example.koview.databinding.FragmentSignupCompleteBinding
+import com.example.koview.presentation.base.BaseFragment
+
+class SignUpCompleteFragment :
+    BaseFragment<FragmentSignupCompleteBinding>(R.layout.fragment_signup_complete) {
+
+}
