df644995f5541c87acadc1aef451f183f125135f
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2024-07-12 00:54:27 +0900

    [13] feat : 쇼핑몰 선택 구현(체크박스를 하나 이상 선택 시에 버튼 활성화, 다음 버튼 클릭 시 체크된 쇼핑몰들 뷰모델에 저장)

--- app/src/main/java/com/example/koview/presentation/ui/intro/signup/selectshop/SignUpSelectShopFragment.kt
+++ app/src/main/java/com/example/koview/presentation/ui/intro/signup/selectshop/SignUpSelectShopFragment.kt
@@ -1,18 +1,25 @@
 package com.example.koview.presentation.ui.intro.signup.selectshop
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.CheckBox
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.koview.R
 import com.example.koview.databinding.FragmentSignupSelectShopBinding
 import com.example.koview.presentation.base.BaseFragment
+import com.example.koview.presentation.ui.main.MainActivity
 
 class SignUpSelectShopFragment :
     BaseFragment<FragmentSignupSelectShopBinding>(R.layout.fragment_signup_select_shop) {
 
     private val viewModel: SignUpSelectShopViewModel by viewModels()
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -20,6 +27,45 @@ class SignUpSelectShopFragment :
         binding.vm = viewModel
 
         initEventObserve()
+
+        val checkboxes = listOf(
+            binding.cbAmazon,
+            binding.cbEBay,
+            binding.cbAliexpress,
+            binding.cbWalmart,
+            binding.cbTarget
+        )
+
+        // 화면 넘어가며 _checkedTexts에 선택한 가게들 업데이트
+        binding.btnSignup.setOnClickListener {
+            findCheckedShop(checkboxes)
+            Log.d("SignUpSelectShopFragment", "Selected Shops String: ${viewModel.getSelectedShopsString()}")
+            viewModel.navigateToNext()
+        }
+
+
+        checkBoxObserve()
+
+    }
+
+    // 체크박스 중 하나라도 체크 될 시를 구독하며 버튼 상태 변경
+    private fun checkBoxObserve(){
+        viewModel.isAnyCheckboxChecked.observe(viewLifecycleOwner){
+            if(it){
+                binding.btnSignup.setBackgroundResource(R.drawable.rect_main3fill_nostroke_12radius)
+                binding.btnSignup.isClickable = true
+            } else {
+                binding.btnSignup.setBackgroundResource(R.drawable.rect_blackfill_nostroke_12radius)
+                binding.btnSignup.isClickable = false
+            }
+
+        }
+    }
+
+    // 최종적으로 체크된 shop들을 뷰모델의 라이브데이터에 저장
+    private fun findCheckedShop(checkboxes: List<CheckBox>) {
+        val checkedShops = checkboxes.filter { it.isChecked }.map { it.text.toString() }
+        viewModel.updateCheckedShops(checkedShops)
     }
 
     private fun initEventObserve() {
--- app/src/main/java/com/example/koview/presentation/ui/intro/signup/selectshop/SignUpSelectShopViewModel.kt
+++ app/src/main/java/com/example/koview/presentation/ui/intro/signup/selectshop/SignUpSelectShopViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.koview.presentation.ui.intro.signup.selectshop
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,6 +21,35 @@ class SignUpSelectShopViewModel @Inject constructor() : ViewModel() {
 
     private val _event = MutableSharedFlow<SelectShopEvent>()
     val event: SharedFlow<SelectShopEvent> = _event.asSharedFlow()
+
+    private val _isAnyCheckboxChecked = MutableLiveData(false)
+    val isAnyCheckboxChecked: LiveData<Boolean> get() = _isAnyCheckboxChecked
+
+    private val _checkedTexts = MutableLiveData<List<String>>()
+    val checkedTexts: LiveData<List<String>> get() = _checkedTexts
+
+    private val checkboxStates = BooleanArray(5) { false }
+
+    // 선택된 상점을 문자열로 변환하는 메서드(나중에 지워야함)
+    fun getSelectedShopsString(): String {
+        return _checkedTexts.value?.joinToString(", ") ?: ""
+    }
+
+
+    // 최종적으로 체크된 shop들을 추출
+    fun updateCheckedShops(shops: List<String>) {
+        _checkedTexts.value = shops
+    }
+
+
+    // 체크박스 중에 체크된 게 있는지 확인
+    fun updateCheckboxState(index: Int, isChecked: Boolean) {
+        checkboxStates[index] = isChecked
+        _isAnyCheckboxChecked.value = checkboxStates.any { it }
+    }
+
+
+
 
     fun navigateToBack() {
         viewModelScope.launch {
