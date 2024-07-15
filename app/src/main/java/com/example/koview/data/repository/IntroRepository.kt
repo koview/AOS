33ed5348b036464eaca1c341c5485bd8319d1432
SeonJeong <sjkim5621@naver.com>
2024-07-16 01:58:31 +0900

    [27] feat : 로그인 및 회원가입 api 세팅

--- app/src/main/java/com/example/koview/data/repository/IntroRepository.kt
+++ app/src/main/java/com/example/koview/data/repository/IntroRepository.kt
@@ -1,4 +1,18 @@
 package com.example.koview.data.repository
 
+import com.example.koview.data.model.BaseState
+import com.example.koview.data.model.requeset.SignInRequest
+import com.example.koview.data.model.requeset.SignUpRequest
+import com.example.koview.data.model.response.SignInResponse
+import com.example.koview.data.model.response.SignUpResponse
+
 interface IntroRepository {
+
+    suspend fun memberSignUp(
+        body: SignUpRequest
+    ): BaseState<SignUpResponse>
+
+    suspend fun memberSignIn(
+        body: SignInRequest
+    ): BaseState<SignInResponse>
 }
