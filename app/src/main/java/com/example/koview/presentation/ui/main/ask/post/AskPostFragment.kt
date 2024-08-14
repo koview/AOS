92225afb8dde42acb80db490525f7dc56325584a
SeonJeong <sjkim5621@naver.com>
2024-08-15 04:37:27 +0900

    [100] feat: 질문 작성 갤러리 연결

--- app/src/main/java/com/example/koview/presentation/ui/main/ask/post/AskPostFragment.kt
+++ app/src/main/java/com/example/koview/presentation/ui/main/ask/post/AskPostFragment.kt
@@ -2,20 +2,33 @@ package com.example.koview.presentation.ui.main.ask.post
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.example.koview.R
 import com.example.koview.databinding.FragmentAskPostBinding
 import com.example.koview.presentation.base.BaseFragment
+import com.example.koview.presentation.ui.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AskPostFragment : BaseFragment<FragmentAskPostBinding>(R.layout.fragment_ask_post) {
 
+    private val parentViewModel: MainViewModel by activityViewModels()
     private val viewModel: AskPostViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.vm = viewModel
+
+        initEventObserver()
+    }
+
+    private fun initEventObserver() {
+        repeatOnStarted {
+            viewModel.event.collect {
+                   parentViewModel.goToSetProfileImage()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/koview/presentation/ui/main/ask/post/AskPostViewModel.kt
+++ app/src/main/java/com/example/koview/presentation/ui/main/ask/post/AskPostViewModel.kt
@@ -1,13 +1,31 @@
 package com.example.koview.presentation.ui.main.ask.post
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+sealed class AskPostEvent() {
+    data object GoToGallery : AskPostEvent()
+}
+
 @HiltViewModel
-class AskPostViewModel @Inject constructor(): ViewModel() {
+class AskPostViewModel @Inject constructor() : ViewModel() {
+
+    private val _event = MutableSharedFlow<AskPostEvent>()
+    val event: SharedFlow<AskPostEvent> = _event.asSharedFlow()
 
     val content = MutableStateFlow("")
+
+    fun goToGallery() {
+        viewModelScope.launch {
+            _event.emit(AskPostEvent.GoToGallery)
+        }
+    }
 
 }
